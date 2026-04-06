d804357b40eba52a3bb47c9a2fe6c2c38b79bf7c
Tim Smith <tim.smith.hdg@gmail.com>
2026-04-06 21:18:28 +0000

    useAppStore commented

--- hooks/useSelection.ts
+++ hooks/useSelection.ts
@@ -1,23 +1,23 @@
-import { useAppStore } from "@/store/useAppStore";
+// import { useAppStore } from "@store/useAppStore";
 
-export function useSelection() {
-  const selectedRepoIds = useAppStore((state) => state.selectedRepoIds);
-  const addRepoId = useAppStore((state) => state.addRepoId);
-  const removeRepoId = useAppStore((state) => state.removeRepoId);
-  const clearSelection = useAppStore((state) => state.clearSelection);
+// export function useSelection() {
+//   const selectedRepoIds = useAppStore((state) => state.selectedRepoIds);
+//   const addRepoId = useAppStore((state) => state.addRepoId);
+//   const removeRepoId = useAppStore((state) => state.removeRepoId);
+//   const clearSelection = useAppStore((state) => state.clearSelection);
 
-  const toggleSelection = (id: string) => {
-    if (selectedRepoIds.includes(id)) {
-      removeRepoId(id);
-    } else {
-      addRepoId(id);
-    }
-  };
+//   const toggleSelection = (id: string) => {
+//     if (selectedRepoIds.includes(id)) {
+//       removeRepoId(id);
+//     } else {
+//       addRepoId(id);
+//     }
+//   };
 
-  return {
-    selectedRepoIds,
-    toggleSelection,
-    clearSelection,
-    isSelected: (id: string) => selectedRepoIds.includes(id),
-  };
-}
+//   return {
+//     selectedRepoIds,
+//     toggleSelection,
+//     clearSelection,
+//     isSelected: (id: string) => selectedRepoIds.includes(id),
+//   };
+// }
